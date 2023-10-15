156c6916cdc7327427f64c3b44f7d32ef027466b
jbalikov <jbalikov@calpoly.edu>
2023-10-14 18:45:06 -0700

    Experimenting with tracks and messages, added note_number_to_name

--- MidiToTabs.py
+++ MidiToTabs.py
@@ -3,6 +3,17 @@ import mido
 from mido import MidiFile
 
 
+def note_number_to_name(note_number):
+    # Define a list of note names
+    note_names = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+    # Calculate the octave and note index
+    octave = note_number // 12
+    note_index = note_number % 12
+    # Get the note name based on the note index
+    note_name = note_names[note_index]
+    return f"{note_name}{octave}"
+
+
 # Splits a given midi song into midi files containing each
 # track separately, saves them in the given dest
 def song_to_tracks(song: MidiFile, dest: str):
@@ -21,20 +32,36 @@ def song_to_tracks(song: MidiFile, dest: str):
 
 
 def main():
+    # Read in our selected midi file
     blinding_lights = MidiFile('AUD_DS1340.mid', clip=True)
+    # Split into tracks
     song_to_tracks(blinding_lights, 'SplitTrackDepot')
+    # We are going to analyze one track within our song
     single_track = MidiFile('SplitTrackDepot/Vocal Guide.mid', clip=True).tracks[0]
-    #print(single_track)
+    # print(single_track)
+    # Print out info about messages within our single track
+    # including whether it was a note on or off, what the note
+    # was, and the message as a whole. We then print the array
+    # of messages sorted by note and then time
+    messages = []
     for message in single_track:
         if type(message) == mido.midifiles.meta.MetaMessage:
             continue
         if message.type == "note_on":
             print("Note Begin!")
+            print("Note Name and Octave: " + note_number_to_name(message.note))
+            messages.append(message)
         elif message.type == "note_off":
             print("Note End!")
+            print("Note Name and Octave: " + note_number_to_name(message.note))
+            messages.append(message)
         else:
             print("Not a Note!")
-        # print(message)
+        print(message)
+    messages = sorted(messages, key=lambda x: (x.note, x.time))
+    print("\n")
+    for message in messages:
+        print(message)
     return 0
 
 

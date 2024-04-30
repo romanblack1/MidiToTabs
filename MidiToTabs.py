cfc425518253c6f1e0f9588ed0cce00429a55e89
Roman <roblack@calpoly.edu>
2024-04-30 12:33:24 -0700

    add get_channel_info()

--- MidiToTabs.py
+++ MidiToTabs.py
@@ -32,6 +32,53 @@ class Tab:
     guitar_note_list: list
 
 
+def get_channel_info(song: MidiFile):
+    instruments = ["Acoustic Grand Piano", "Bright Acoustic Piano", "Electric Grand Piano", "Honky-tonk Piano",
+                   "Electric Piano 1", "Electric Piano 2", "Harpsichord", "Clavi", "Celesta", "Glockenspiel",
+                   "Music Box", "Vibraphone", "Marimba", "Xylophone", "Tubular Bells", "Dulcimer", "Drawbar Organ",
+                   "Percussive Organ", "Rock Organ", "Church Organ", "Reed Organ", "Accordion", "Harmonica",
+                   "Tango Accordion", "Acoustic Guitar (nylon)", "Acoustic Guitar (steel)",
+                   "Electric Guitar (jazz)", "Electric Guitar (clean)", "Electric Guitar (muted)",
+                   "Overdriven Guitar", "Distortion Guitar", "Guitar Harmonics", "Acoustic Bass",
+                   "Electric Bass (finger)", "Electric Bass (pick)", "Fretless Bass", "Slap Bass 1", "Slap Bass 2",
+                   "Synth Bass 1", "Synth Bass 2", "Violin", "Viola", "Cello", "Contrabass", "Tremolo Strings",
+                   "Pizzicato Strings", "Orchestral Harp", "Timpani", "String Ensemble 1", "String Ensemble 2",
+                   "Synth Strings 1", "Synth Strings 2", "Choir Aahs", "Voice Oohs", "Synth Voice", "Orchestra Hit",
+                   "Trumpet", "Trombone", "Tuba", "Muted Trumpet", "French Horn", "Brass Section", "Synth Brass 1",
+                   "Synth Brass 2", "Soprano Sax", "Alto Sax", "Tenor Sax", "Baritone Sax", "Oboe", "English Horn",
+                   "Bassoon", "Clarinet", "Piccolo", "Flute", "Recorder", "Pan Flute", "Blown bottle", "Shakuhachi",
+                   "Whistle", "Ocarina", "Lead 1 (square)", "Lead 2 (sawtooth)", "Lead 3 (calliope)",
+                   "Lead 4 (chiff)", "Lead 5 (charang)", "Lead 6 (voice)", "Lead 7 (fifths)",
+                   "Lead 8 (bass + lead)", "Pad 1 (new age)", "Pad 2 (warm)", "Pad 3 (polysynth)", "Pad 4 (choir)",
+                   "Pad 5 (bowed)", "Pad 6 (metallic)", "Pad 7 (halo)", "Pad 8 (sweep)", "FX 1 (rain)",
+                   "FX 2 (soundtrack)", "FX 3 (crystal)", "FX 4 (atmosphere)", "FX 5 (brightness)",
+                   "FX 6 (goblins)", "FX 7 (echoes)", "FX 8 (sci-fi)", "Sitar", "Banjo", "Shamisen", "Koto",
+                   "Kalimba", "Bag pipe", "Fiddle", "Shanai", "Tinkle Bell", "Agogô", "Steel Drums", "Woodblock",
+                   "Taiko Drum", "Melodic Tom", "Synth Drum", "Reverse Cymbal", "Guitar Fret Noise", "Breath Noise",
+                   "Seashore", "Bird Tweet", "Telephone Ring", "Helicopter", "Applause", "Gunshot"]
+    channels_dict = {}
+    channels_instr = {}
+    for track_index in range(len(song.tracks)):
+        total_time = 0
+        for message in song.tracks[track_index]:
+            total_time += message.time
+            if type(message) == mido.messages.messages.Message and message.type == "program_change":
+                channels_instr[message.channel] = instruments[message.program]
+            if type(message) == mido.messages.messages.Message and message.type == 'note_on':
+                channels_dict.setdefault(message.channel, [])
+                channels_dict[message.channel].append(message)
+
+    non_empty_channels = []
+    for channel in channels_dict:
+        if channel == 9:
+            non_empty_channels.append(("Drums", channels_dict[channel]))
+            continue
+        non_empty_channels.append((channels_instr[channel], len(channels_dict[channel])))
+
+    returned_channels = sorted(non_empty_channels, key=lambda x: x[1], reverse=True)
+    return returned_channels
+
+
 # Given tuning and capo offsets, create a dictionary of notes to fret-string combos
 def create_guitar_index(tuning_offset, capo_offset):
     e_string = (64 + capo_offset, 81)
